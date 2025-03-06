0330f9d07897f9bc351336404cbb44dd0c33e7b8
gr-im <grimfw@gmail.com>
2025-03-06 14:29:32 +0100

    Use virtfs instead of a dummy handler in jsonrpc test

--- test/server/jsonrpc_test.ml
+++ test/server/jsonrpc_test.ml
@@ -1,21 +1,16 @@
 open Kohai_core
 open Kohai_server
 
-module Handler = Eff.Handler (struct
-    let exists _ = true
-    let is_file _ = true
-    let is_dir _ = true
-    let read_file _ = ""
-    let create_dir _ = ()
-    let write_file _ _ = ()
-    let append_to_file _ _ = ()
-    let delete_file _ = ()
-    let delete_dir ?recursive:_ _ = ()
-    let now () = 0.0
-    let datetime_from_float _ = Ok Datetime.unix
-    let set_supervised_directory _ = ()
-    let get_supervised_directory () = None
+module F = Virtfs.Make (struct
+    let fs = Virtfs.(from_list [ dir ".kohai" [] ])
+
+    let now =
+      Datetime.(make ~time:(10, 0, 0) ~year:2025 ~month:Mar ~day:1 ())
+      |> Result.get_ok
+    ;;
   end)
+
+module Handler = Eff.Handler (F)
 
 let dump = function
   | Ok value -> Fmt.str "%a" Rensai.Ast.pp value

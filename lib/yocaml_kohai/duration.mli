8f1c6e3c934ce2c6d928e9fae84c0b93daadfa2a
xvw <xavier.vdw@gmail.com>
2025-04-03 17:00:19 +0200

    Remove `yocaml_kohai` ATM

--- lib/yocaml_kohai/duration.mli
+++
@@ -1,4 +0,0 @@
-type t = Kohai_core.Duration.t
-
-val validate : Yocaml.Data.t -> t Yocaml.Data.Validation.validated_value
-val normalize : t -> Yocaml.Data.t
--- lib/yocaml_kohai/state.mli
+++
@@ -1,4 +0,0 @@
-type t = Kohai_model.State.t
-
-val validate : Yocaml.Data.t -> t Yocaml.Data.Validation.validated_value
-val normalize : t -> Yocaml.Data.t

f9c014251488bc2ed2326ef90d38219942fab6bc
xvw <xavier.vdw@gmail.com>
2025-05-18 23:43:44 +0200

    Lift UUID Set as a metadata bucket

--- lib/yocaml_kohai/uuid.ml
+++ lib/yocaml_kohai/uuid.ml
@@ -14,6 +14,9 @@ let normalize id = id |> Kohai_core.Uuid.to_string |> Yocaml.Data.string
 module Set = struct
   type t = Kohai_core.Uuid.Set.t
 
+  let entity_name = "Kohai.Uuid.Set"
+  let neutral = Yocaml.Metadata.required entity_name
+
   let validate =
     let open Yocaml.Data.Validation in
     (list_of validate $ Kohai_core.Uuid.Set.from_list)
--- lib/yocaml_kohai/uuid.mli
+++ lib/yocaml_kohai/uuid.mli
@@ -6,6 +6,7 @@ val normalize : t -> Yocaml.Data.t
 module Set : sig
   type t = Kohai_core.Uuid.Set.t
 
-  val validate : Yocaml.Data.t -> t Yocaml.Data.Validation.validated_value
+  include Yocaml.Required.DATA_READABLE with type t := t
+
   val normalize : t -> Yocaml.Data.t
 end
